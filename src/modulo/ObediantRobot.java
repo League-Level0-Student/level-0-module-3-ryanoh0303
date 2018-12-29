e220f2f38080f08a1fdbef3ee65f08b4617b35cf
ryanoh0303 <42789996+ryanoh0303@users.noreply.github.com>
2018-12-28 21:09:39 -0800

    Update ObediantRobot.java

--- src/modulo/ObediantRobot.java
+++ src/modulo/ObediantRobot.java
@@ -1,18 +1,32 @@
 package modulo;
 import org.jointheleague.graphical.robot.Robot;
+import javax.swing.JOptionPane;
 public class ObediantRobot {
 
 	public static void main(String[] args) {
-	    Robot rob= new Robot();
-	    rob.penDown();
-	    rob.setSpeed(50);
+		 Robot rob= new Robot();
+         rob.penDown();
+		 rob.setSpeed(50);
+		String y=JOptionPane.showInputDialog("Which shape do you want?");
+		if(y.equalsIgnoreCase("Triangle")) {
+			ObediantRobot main= new ObediantRobot();
+			main.drawTriangle(rob);
+		}
+		else if(y.equalsIgnoreCase("Square")) {
+			ObediantRobot main= new ObediantRobot();
+			main.DrawSquare(rob);
+		}
+		else if(y.equalsIgnoreCase("Circle")) {
+			 ObediantRobot oh= new ObediantRobot();
+			    oh.drawCircle(rob);
+		}
+	   
 	    //ObediantRobot ryan= new ObediantRobot();
 	    //ryan.DrawSquare(rob);
 	   // ObediantRobot ray= new ObediantRobot();
 	    //ray.drawTriangle(rob);
 	    
-	    ObediantRobot oh= new ObediantRobot();
-	    oh.drawCircle(rob);
+	   
 
 	}
 	public void DrawSquare(Robot rob) {
@@ -30,7 +44,7 @@ public class ObediantRobot {
 	public void drawCircle(Robot rob) {
 		//I am stuck on this part. I do not know how to draw a circle. 
 		for(int i=0; i<3; i--) {
-			rob.turn
+			rob.turn(360);
 		}
 	}
 
